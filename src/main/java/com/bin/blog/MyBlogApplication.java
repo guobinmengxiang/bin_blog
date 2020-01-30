7a292bea1f45f9251f06e6efdff4d4063a3b169f
zhangguobin <1774427221@qq.com>
2020-01-30 19:12:33 +0800

    pringboot整合druid实现监控功能

--- src/main/java/com/bin/blog/MyBlogApplication.java
+++ src/main/java/com/bin/blog/MyBlogApplication.java
@@ -3,10 +3,12 @@ package com.bin.blog;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 //其注册到 Spring 的 IOC 容器中以供其他类调用
 //添加 @MapperScan 注解将相应包下的所有 Mapper 接口扫描到容器中
 @MapperScan("com.bin.blog.dao")
 @SpringBootApplication
+@ServletComponentScan
 public class MyBlogApplication {
     public static void main(String[] args) {
         SpringApplication.run(MyBlogApplication.class, args);
